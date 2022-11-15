16d63e520b5c7cfabf842b279c8f576b647e8c33
Kamil Dzierzbicki <kdzierzb@unitygroup.com>
2022-11-15 23:43:06 +0100

    nauka toRefs

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,37 +1,46 @@
 <template>
 
   <div>
-
-  <h1>Price: {{price}}</h1>
+  <!-- jeżeli używasz toReds piszemy wtedy zmienne bez state -->
+  <h1>Price: {{state.price}}$</h1>
     <button @click="makeOrder">Make next order</button>
 
-    <p>quantity: {{quantity}}</p>
-    <p>Order total price: {{totalPrice}}</p>
-    <p>Tax: {{tax}}</p>
+    <p>quantity: {{state.quantity}}</p>
+    <p>Order total price: {{state.totalPrice}}</p>
+    <p>Tax: {{state.tax}}</p>
 
   </div>
 
 </template>
 
 <script>
-import {ref} from 'vue'
+import {reactive} from 'vue'
+//import {reactive, toRefs} from 'vue'
 export default {
   name: "MyComponent",
 
   setup() {
 
-    const quantity = ref(0);
-    const price = ref(100);
-    const totalPrice = ref(0);
-    const tax = ref(0);
+      const state = reactive({
 
-      function makeOrder(){
+        quantity: 0,
+        price: 100,
+        totalPrice: 0,
+        tax: 0,
+      });
 
-        quantity.value++;
-        totalPrice.value = quantity.value * price.value;
-        tax.value = totalPrice.value * 0.23;
-      }
-    return{makeOrder, quantity, price, totalPrice, tax,};
+    function makeOrder(){
+
+      state.quantity++;
+      state.totalPrice = state.quantity * state.price;
+      state.tax= state.totalPrice * 0.23;
+    }
+
+
+
+    return{makeOrder, state};
+
+      //return{makeOrder, ...toRefs(state)};
   }
 
   };
